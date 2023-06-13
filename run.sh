0fdd76a544a4a20ea1ccc7d8e7a66664357c708f
Sepehr Mousavi <mousavi.sep@gmail.com>
2023-06-13 18:08:23 +0200

    add run shell script

---
+++ run.sh
@@ -0,0 +1,17 @@
+datadir='./data/230601/base'
+Ls=(1 5 10 15 20 30 40)
+Ds=(1)
+Ws=(10 20 50 )
+
+for D in ${Ds[*]}; do
+    for W in ${Ws[*]}; do
+        for L in ${Ls[*]}; do
+            python run.py \
+                --datadir $datadir --n_trn 2048 --n_val 512 \
+                -L $L \
+                -D $D -W $W \
+                --epochs 1e03 --lr 5e-04 \
+                --normalize-output
+        done
+    done
+done
